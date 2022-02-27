3ffda0921c4fea224df9bb78045d7d1d352fcb77
Olga Zhidkova <zhidkova.olga12@gmail.com>
2022-02-27 14:15:51 +0300

    Initial Commit

---
+++ HW 20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 20
+//
+//  Created by Ольга on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
