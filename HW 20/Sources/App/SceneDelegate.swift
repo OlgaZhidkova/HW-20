7e9ef5e39cb8763cd74d8f6a7231060d5c887a23
Olga Zhidkova <zhidkova.olga12@gmail.com>
2022-03-27 18:10:54 +0300

    Use Operation queue.

---
+++ HW 20/Sources/App/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  HW 20
+//
+//  Created by Ольга on 27.02.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
